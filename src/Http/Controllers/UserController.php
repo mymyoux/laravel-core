de54a2217f64d79bebbae725b13ff221408abe6d
mymyoux <mymyoux@gmail.com>
2018-12-02 18:09:48 +0100

    add user helper methods

--- src/Http/Controllers/UserController.php
+++ src/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Core\Api\Annotations as myno;
 use Auth;
 use App\User;
 use Storage;
+use Illuminate\Http\Request;
 class UserController extends Controller
 {
     /**
@@ -15,6 +16,39 @@ class UserController extends Controller
     public function me()
     {
         return Auth::user();
+    }
+     /**
+     * @myno\Param(name="email", required=true, requirements="email")
+     */
+    public function emailExists($email)
+    {
+        $req = User::where(function($query) use($email)
+        {
+            $query->where('email', '=', $email);
+            $query->orWhereHas('connectors',  function ($query) use($email) {
+                $query->where('email', '=', $email);
+            });
+        });
+        if(Auth::check())
+            $req->where('id', '!=', Auth::id());
+        return !!$req->first();
+    }
+     /**
+     * @myno\Role("user")
+     * @myno\Param(name="email",  requirements="email")
+     * @myno\Param(name="name")
+     */
+    public function update(Request $request, $email, $name)
+    {
+        $data = $request->only('email', 'name');
+        
+        $user = Auth::user();
+        foreach($data as $key=>$value)
+        {
+            $user->$key = $value;
+        }
+        $user->save();
+        return $user;
     }
     public function onSignup($event)
     {
